399e668c9bbac37f6afdb5f7c1bb0442079acec9
Tom Read-Cutting <readcuttingt@gmail.com>
2015-12-12 21:23:24 +0000

    Added gulp-stylus and gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('styles', function() {
+  // Get one .styl file and render 
+  gulp.src('./css/*.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', function() {
+  var args = ['styles'];
+
+  //sequence.apply(null, args);
 });
