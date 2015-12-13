4de69c9000f173f9e0281ed68a09bfd94cedbacb
Tom Read-Cutting <readcuttingt@gmail.com>
2015-12-13 00:27:12 +0000

    Created a proper build system so that .styl files will be compiled to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var runSequence = require('run-sequence');
+var del = require('del');
 
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
+gulp.task('clean', function () {
+  return del('public');
+});
+
+
+var assetPath = ['./assets/**', '!./assets/stylesheets/*.styl'];
+
 gulp.task('styles', function() {
   // Get one .styl file and render 
-  gulp.src('./assets/css/*.styl')
+  gulp.src('./assets/stylesheets/*.styl')
     .pipe(stylus({
       'include css': true
     }))
-    .pipe(gulp.dest('./pubilic/css/'));
+    .pipe(gulp.dest('./public/stylesheets'));
+});
+
+// other assets
+gulp.task('assets', function () {
+  gulp.src(assetPath)
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('build', function() {
-  var args = ['styles'];
+  var args = ['clean', 'styles', 'assets'];
 
-  //sequence.apply(null, args);
+  runSequence.apply(null, args);
 });
