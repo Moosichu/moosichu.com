e021e2b9ee6a203b66a30b311c99fe0246ef1abb
Tom Read-Cutting <readcuttingt@gmail.com>
2015-12-16 20:38:30 +0000

    Added social-media logos to on the web navbar.

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get(['/', '/index'], function(req, res, next) {
-  res.render('index', { title: 'Moosichu.com' });
+  res.render('index', {
+    title: 'Moosichu.com',
+    fs: require('fs')
+  });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Moosichu.com' });
+  res.render('about', {
+    title: 'Moosichu.com',
+    fs: require('fs')
+  });
 });
 
 module.exports = router;
