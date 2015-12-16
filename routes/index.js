8491c0a45c4aa5d4f0d84229e1bbe754aa33be9c
Tom Read-Cutting <readcuttingt@gmail.com>
2015-12-16 23:34:03 +0000

    Embedded blog on website.

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get(['/', '/index'], function(req, res, next) {
   res.render('index', {
-    title: 'Moosichu.com',
-    fs: require('fs')
+    title: 'Moosichu.com'
   });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about', {
-    title: 'Moosichu.com',
-    fs: require('fs')
+    title: 'Moosichu.com'
+  });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', {
+    title: 'Moosichu.com'
   });
 });
 
