066322435f33c0a50ab442389dd34d510e7a0600
Tom Read-Cutting <readcuttingt@gmail.com>
2015-12-18 10:52:42 +0000

    Added new test to test http requests and responses.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+var expect = require('chai').expect;
+var request = require('request');
+
+describe("Color Code Converter API", function() {
+  describe("RGB to Hex conversion", function() {
+    var url = "http://localhost:3000/cv.pdf";
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
   });
 });
